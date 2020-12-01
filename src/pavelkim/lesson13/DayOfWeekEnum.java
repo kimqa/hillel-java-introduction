f74e1f670ca0b96859a1b2b8f06ebeb88c044df5
kim-alterpost <kim@alterpost.org>
2020-12-01 07:05:48 +0200

    LESSON13

---
+++ src/pavelkim/lesson13/DayOfWeekEnum.java
@@ -0,0 +1,15 @@
+package pavelkim.lesson13;
+
+public enum DayOfWeekEnum {
+//    private int workingHours;
+//
+//    MONDAY(workingHours: 8),TUESDAY(workingHours: 8), WEDNESDAY(workingHours: 8), SUTURDAY(workingHours: 8), дьыFRYDAY(workingHours:8)
+//
+//    DayOfWeekEnum(int workingHours) {
+//        this.workingHours = workingHours;
+//    }
+//
+//    public int getWorkingHours() {
+//        return workingHours;
+//    }
+}
