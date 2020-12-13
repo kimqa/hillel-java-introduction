e5a39e461cbcc4d575412809f2e9c71532fb475a
kim-alterpost <kim@alterpost.org>
2020-12-13 23:22:03 +0200

    ДЗ 13. Exceptions

--- src/pavelkim/homework11/NotEnoughMoneyException.java
+++ src/pavelkim/homework11/NotEnoughMoneyException.java
@@ -2,7 +2,7 @@ package pavelkim.homework11;
 
 public class NotEnoughMoneyException extends RuntimeException {
 
-
-
-
+    public NotEnoughMoneyException(String detail){
+        System.out.println(detail);
+    }
 }
